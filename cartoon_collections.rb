90c8a4b001781d2460ce93c706fcdb460efd1e29
Brian Sahota <bpsahota@gmail.com>
2020-09-08 20:10:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,39 @@
-def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
-end
 
-def summon_captain_planet(planeteer_calls)
+def square_array(array)
+  array.map do |element|
+    element * element 
+  end 
+  end
+
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |element|
+  "#{element.capitalize}!"
+end
 end
 
-def long_planeteer_calls(planeteer_calls)
-  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
+# Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-end
+  
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? do |element|
+    element.size > 4
+  end
+end 
+
+# Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
+  # Return the first valid call found, or return nil if no valid calls are found
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
-  # Return the first valid call found, or return nil if no valid calls are found
-end
+  result = nil
+  planeteer_calls.find do |element|
+    element = valid_calls.include?(element)
+  end 
+end 
+    
+  #found = array1.find( val => array2.include?(val) )
